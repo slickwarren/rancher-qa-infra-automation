ddc08ce22cbe3cbd5468d81d30922461a81b08fe
Caleb Warren <caleb.warren@suse.com>
2026-03-18 14:23:05 -0700

    fix formatting

--- tofu/rancher/custom_cluster/outputs.tf
+++ tofu/rancher/custom_cluster/outputs.tf
@@ -1,5 +1,5 @@
 output "cluster_registration_token" {
-  value     = data.rancher2_cluster_v2.rancher2_cluster_v2.cluster_registration_token[0]["insecure_node_command"]
+  value = data.rancher2_cluster_v2.rancher2_cluster_v2.cluster_registration_token[0]["insecure_node_command"]
   sensitive = true
 }
 
